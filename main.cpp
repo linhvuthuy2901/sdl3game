f112fcdfd8a4c18ec039c20e4f73cf818faf37bc
linhvuthuy2901 <24021548@vnu.edu.vn>
2025-02-23 16:44:40 +0700

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,53 +1,33 @@
-#define SDL_MAIN_USE_CALLBACKS 1
-#include <SDL3/SDL.h>
-#include <SDL3/SDL_main.h>
+#include "SDL3/SDL_video.h"
+#include "sdl3/SDL_render.h"
+#include <cstddef>
+#define SDL_MAIN_USE_CALLBACKS
+#include <sdl3/sdl.h>
+#include <sdl3/sdl_main.h>
 
-#define WINDOW_WIDTH  800
-#define WINDOW_HEIGHT 600
+SDL_Window* window;
+SDL_Renderer* renderer;
 
-typedef struct {
-    SDL_Window *window;
-    SDL_Renderer *renderer;
-} AppState;
+void SDL_AppQuit(void *appstate, SDL_AppResult result)
+{
 
-SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
-    if (!SDL_Init(SDL_INIT_VIDEO)) {
-        return SDL_APP_FAILURE;
-    }
-
-    AppState *as = (AppState *)SDL_calloc(1, sizeof(AppState));
-    if (!as) {
-        return SDL_APP_FAILURE;
-    }
-    *appstate = as;
-
-    if (!SDL_CreateWindowAndRenderer("SDL3 Window", WINDOW_WIDTH, WINDOW_HEIGHT, 0, &as->window, &as->renderer)) {
-        return SDL_APP_FAILURE;
-    }
-    
-    return SDL_APP_CONTINUE;
 }
 
-SDL_AppResult SDL_AppIterate(void *appstate) {
-    AppState *as = (AppState *)appstate;
-    SDL_SetRenderDrawColor(as->renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-    SDL_RenderClear(as->renderer);
-    SDL_RenderPresent(as->renderer);
-    return SDL_APP_CONTINUE;
-}
-
-SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
-    if (event->type == SDL_EVENT_QUIT) {
-        return SDL_APP_SUCCESS;
-    }
-    return SDL_APP_CONTINUE;
-}
-
-void SDL_AppQuit(void *appstate, SDL_AppResult result) {
-    if (appstate != NULL) {
-        AppState *as = (AppState *)appstate;
-        SDL_DestroyRenderer(as->renderer);
-        SDL_DestroyWindow(as->window);
-        SDL_free(as);
-    }
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char argv[])
+{
+        if(!SDL_Init(SDL_INIT_VIDEO))
+        {
+            SDL_Log("Error initializing SDL: %s", SDL_GetError());
+        }
+        return SDL_APP_FAILURE;
+        window = SDL_CreateWindow("Snake Game toi yeu", 900, 800, NULL);
+        if(!window)
+        {
+            SDL_Log("Error creating window: %s", SDL_GetError());
+        }
+        renderer = SDL_CreateRenderer(window, NULL);
+        if(!renderer)
+        {
+            SDL_Log("Error creating renderer: %s", SDL_GetError());
+        }
 }
