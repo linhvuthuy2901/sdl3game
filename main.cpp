063cb28630988c7d244d6a315579c08bca79daa6
linhvuthuy2901 <24021548@vnu.edu.vn>
2025-02-23 21:06:48 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include "SDL3/SDL_init.h"
+#include "SDL3/SDL_video.h"
+#include "sdl3/SDL_events.h"
+#include "sdl3/SDL_render.h"
+#include <cstddef>
+#define SDL_MAIN_USE_CALLBACKS
+#include <sdl3/sdl.h>
+#include <sdl3/sdl_main.h>
+
+SDL_Window* window;
+SDL_Renderer* renderer;
+
+void SDL_AppQuit(void *appstate, SDL_AppResult result)
+{
+
+}
+
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv)
+{
+        if(!SDL_Init(SDL_INIT_VIDEO))
+        {
+            SDL_Log("Error initializing SDL: %s", SDL_GetError());
+            return SDL_APP_FAILURE;
+        }
+        window = SDL_CreateWindow("Snake Game toi yeu",800,600,NULL);
+   
+        if(!window)
+        {
+            SDL_Log("Error creating window: %s", SDL_GetError());
+            return SDL_APP_FAILURE;
+        }
+        
+        renderer = SDL_CreateRenderer(window, NULL);
+        if(!renderer)
+        {
+            SDL_Log("Error creating renderer: %s", SDL_GetError());
+            return SDL_APP_FAILURE;
+        }
+        return SDL_APP_CONTINUE;
+}
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
+{
+    return SDL_APP_CONTINUE;
+}
+SDL_AppResult SDL_AppIterate(void *appstate)
+{
+    return SDL_APP_CONTINUE;
+}
