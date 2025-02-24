63157aa7f60e05f6e9db1a670dba4c6206356209
linhvuthuy2901 <24021548@vnu.edu.vn>
2025-02-24 11:49:42 +0700

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "SDL3/SDL_init.h"
+#include "SDL3/SDL_oldnames.h"
 #include "SDL3/SDL_video.h"
 #include "sdl3/SDL_events.h"
 #include "sdl3/SDL_render.h"
@@ -12,7 +13,11 @@ SDL_Renderer* renderer;
 
 void SDL_AppQuit(void *appstate, SDL_AppResult result)
 {
-
+    SDL_DestroyRenderer(renderer);
+    renderer = NULL;
+    SDL_DestroyWindow(window);
+    window=NULL;
+    SDL_QuitSubSystem(SDL_INIT_VIDEO);
 }
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv)
@@ -48,5 +53,22 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 }
 SDL_AppResult SDL_AppIterate(void *appstate)
 {
+    SDL_SetRenderDrawColor(renderer,255, 182, 193, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+    
+    
+    SDL_SetRenderDrawColor(renderer,0, 182, 193, 255);
+    for (int x = 0 ; x < 800; x += 40)
+    {
+        SDL_RenderLine(renderer, x, 0, x, 600);
+    }
+    SDL_RenderPresent(renderer);
+    SDL_SetRenderDrawColor(renderer,0, 182, 193, 255);
+    for (int y = 0 ; y < 600 ; y += 40)
+    {
+        SDL_RenderLine(renderer, 0, y, 800, y);
+    }
+    SDL_RenderPresent(renderer);
     return SDL_APP_CONTINUE;
 }
